08ce588c1fee041921c618ebb82601f5f3e2bc8b
Nischal Tuladhar <nischaltuladhar15@gmail.com>
2021-12-12 18:23:06 +0545

    implementing OOP model

--- react-laravel/src/AdminPannel.js
+++ react-laravel/src/AdminPannel.js
@@ -1,14 +1,13 @@
 import Header from './Header';
 import { useEffect,useState } from 'react';
 import { useHistory } from 'react-router';
-import DeleteItem from './DeleteItem';
 import { Link } from 'react-router-dom';
-import ReactPaginate from 'react-paginate';
+import Pagi from './Pagi';
+
 function AdminPannel() {
     const user = JSON.parse(localStorage.getItem('user-info'));
     let history = new useHistory();
     let [data,setData] = useState([]);
-    let [uid,setUid] = useState('');
     useEffect(async ()=>{
         if(user && user.id !== 1) {
             history.push('/');
@@ -16,152 +15,16 @@ function AdminPannel() {
             let result = await fetch("http://127.0.0.1:8000/api/adminList");
             result = await result.json();
             setData(result);
-            // console.log(data.products);
         }
     },[])
 
-    function showAreYouSure(id) {
-        let sure = document.getElementById('sure');
-        let sureBody = document.getElementById('sureBody');
-        let surU = document.getElementById('surU');
-        let body = document.getElementById('body');
-        sure.style.display = "block";
-        surU.style.display = 'block';
-        sureBody.style.display = "block";
-        body.style.overflowY = "hidden";
-        setUid(id);
-       
-    }
-    function showAreYouSureP(id) {
-        let sure = document.getElementById('sure');
-        let surP = document.getElementById('surP');
-        let sureBody = document.getElementById('sureBody');
-        let body = document.getElementById('body');
-        sure.style.display = "block";
-        sureBody.style.display = "block";
-        surP.style.display = "block";
-        body.style.overflowY = "hidden";
-        setUid(id);
-       
-    }
-    function showAreYouSureC(id) {
-        let sure = document.getElementById('sure');
-        let surP = document.getElementById('surP');
-        let sureBody = document.getElementById('sureBody');
-        let body = document.getElementById('body');
-        let surC = document.getElementById('surC');
-        surC.style.display = 'block';
-        sure.style.display = "block";
-        sureBody.style.display = "block";
-        body.style.overflowY = "hidden";
-        setUid(id);
-       
-    }
-    function hideAreYouSure() {
-        let sure = document.getElementById('sure');
-        let sureBody = document.getElementById('sureBody');
-        let surP = document.getElementById('surP');
-        let surU = document.getElementById('surU');
-        let surC = document.getElementById('surC');
-        let body = document.getElementById('body');
-        surC.style.display='none';
-        sure.style.display = "none";
-        surP.style.display = "none";
-        sureBody.style.display = "none";
-        surU.style.display = "none";
-        body.style.overflowY= "scroll";
-        setUid('');
-    } 
-    async function deleteUser() {
-        let result = await fetch("http://127.0.0.1:8000/api/deleteUser/"+uid, {
-            method:'DELETE'
-        });
-        result = await result.json();
-        if(result.msg === 'success') {
-            window.location.reload();
-        } else {
-            alert(result.msg);
-        }
-    } async function deleteCart() {
-        let result = await fetch("http://127.0.0.1:8000/api/delCart/"+uid, {
-            method:'DELETE'
-        });
-        result = await result.json();
-        if(result.msg === 'success') {
-            window.location.reload();
-        } else {
-            alert(result.msg);
-            hideAreYouSure();
-        }
-    }
-    const [pageNo,setPageNo] = useState(0);
-    const disAmount = 4;
-    const viewedPages = pageNo * disAmount;
-    let displayCont;
-    let totalNo;
-    if(data.msg === 'success') {
-        displayCont = data.products.slice(viewedPages,viewedPages+disAmount).map((product)=>{
-            return(
-            <tr>
-                <td>
-                    <img src={"http://127.0.0.1:8000/"+product.file_path} /><br />
-                    <span style={{'fontSize':'10pt','color':'rgba(0,0,0,0.6)'}}>{product.name}</span>
-                </td>
-                <td>{product.description}</td>
-                <td>Rs.{product.price}</td>
-                <td>
-                <button className="btn btn-danger" onClick={()=>showAreYouSureP(product.id)} style={{'marginRight':'.5em'}}>Delete</button>
-                    <Link to={"/updateProduct/"+product.id} className="btn btn-secondary" style={{'marginTop':'.2em','width':'5em'}}>Update</Link>
-                </td>
-            </tr>
-        )});
-        
-        totalNo = Math.ceil(data.products.length/disAmount);
-
-    } else {
-        totalNo = 0;
-    }
-
-    let changePage = ({selected}) => {
-        setPageNo(selected);
-    }
     return(
         <>
-            <Header />
-            <div id='sureBody' className="sureBoxBody"></div>
-            <div class="modal" tabindex="-1" id="sure">
-            <div class="modal-dialog">
-                <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title">You Sure?</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close" onClick={hideAreYouSure}></button>
-                </div>
-                <div class="modal-body">
-                    <p>Do you really want to delete the user?</p>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal" onClick={hideAreYouSure}>No</button>
-                    <button type="button" class="btn btn-primary" onClick={deleteUser} id="surU">Yes</button>
-                    <button type="button" class="btn btn-primary" onClick={deleteCart} id="surC">Sure</button>
-                    <DeleteItem itm={uid} />
-                    
-                </div>
-                </div>
-            </div>
-            </div>
-            {/* <div id='sure' className="sureBox">
-                <div style={{'display':'block','width':'100%','backgroundColor':'#ccc','height':'30px','borderRadius':'12px 12px 0 0'}}><span className="sureX" onClick={hideAreYouSure}>X</span></div>
-                <div style={{'padding':'3em 2em','fontSize':'15pt'}}>
-                    Are u sure you want to delete this user ?
-                    <div style={{'position':'absolute','right':'1.5em','bottom':'1em'}}>
-                        <button className="btn btn-danger" onClick={deleteUser}>Delete</button>
-                    </div>
-                </div>
-            </div> */}
-            
+            <Header />            
             <div className="container">
             <h1 align='center' style={{'paddingBottom':'1em'}}>Users</h1>
             { data.msg === 'success' && data.usr !== 'empty' ?
+            <div>
                 <table className="table">
                     <thead>
                     <tr>
@@ -174,21 +37,13 @@ function AdminPannel() {
                     </tr>
                     </thead>
                     <tbody>
-                    {
-                        data.users.map((user)=>
-                         user.id === 1 ? null :
-                        <tr>
-                            <td>{user.id}</td>
-                            <td>{user.name}</td>
-                            <td>{user.email}</td>
-                            <td>{user.username}</td>
-                            <td>{user.created_at}</td>
-                            <td><button className='btn btn-danger' onClick={()=>showAreYouSure(user.id)}>Delete</button></td>
-                        </tr>
-                        )
-                    }
+                    
+                <Pagi user={data.users} itm="user" />
                 </tbody>
-                </table> 
+                </table>
+                
+                
+                </div>
                 : <div className="emptyMsg" >Nothing Here</div>
                 }
                 <h1 align='center' style={{'paddingBottom':'1em'}}>Products <Link to="/addProduct" className="btn btn-primary">Add</Link> </h1>
@@ -203,42 +58,11 @@ function AdminPannel() {
                     </tr>
                     </thead>
                     <tbody>
-                    { displayCont
-                        // data.products.map((product)=>
-                        // <tr>
-                        //     <td>
-                        //         <img src={"http://127.0.0.1:8000/"+product.file_path} /><br />
-                        //         <span style={{'fontSize':'10pt','color':'rgba(0,0,0,0.6)'}}>{product.name}</span>
-                        //     </td>
-                        //     <td>{product.description}</td>
-                        //     <td>Rs.{product.price}</td>
-                        //     <td>
-                        //     <button className="btn btn-danger" onClick={()=>showAreYouSureP(product.id)} style={{'marginRight':'.5em'}}>Delete</button>
-                        //         <Link to={"/updateProduct/"+product.id} className="btn btn-secondary" style={{'marginTop':'.2em','width':'5em'}}>Update</Link>
-                        //     </td>
-                        // </tr>
-                        // )
-                    }
+                    {/* { displayCont } */}
+                    <Pagi user={data.products} itm="product" />
                     </tbody>
                     </table> 
                     : <div className="emptyMsg" >Nothing Here</div>
-                }
-                {  totalNo > 1 ?
-                    <div style={{'display':'flex','justifyContent':'center','paddingBottom':'.5em'}}>
-                    <ReactPaginate
-                    previousLabel={"Previous"}
-                    nextLabel = {"Next"}
-                    pageCount = {totalNo}
-                    onPageChange = {changePage}
-                    containerClassName = {"pagination page-item"}
-                    previousLinkClassName = {"page-link"}
-                    nextLinkClassName = {"page-link"}
-                    disabledClassName = {"page-item dissabled"}
-                    disabledLinkClassName = {"page-link"}
-                    activeClassName = {"page-item active"}
-                    pageClassName = {"page-item"}
-                    pageLinkClassName = {"page-link"}
-                    /></div>: null
                 }
 
                     <h1 align='center' style={{'paddingBottom':'1em'}}>Carts</h1>
@@ -255,23 +79,8 @@ function AdminPannel() {
                         </tr>
                         </thead>
                         <tbody>
-                        {
-                            data.carts.map((cart)=>
-                            <tr>
-                                <td>
-                                    <img src={"http://127.0.0.1:8000/"+cart.file_path} /><br />
-                                    <span style={{'fontSize':'10pt','color':'rgba(0,0,0,0.6)'}}>{cart.name}</span>
-                                </td>
-                                <td>{cart.user_id}</td>
-                                <td>{cart.description}</td>
-                                <td>{cart.created_at}</td>
-                                <td>Rs.{cart.price}</td>
-                                <td>
-                                <button className="btn btn-danger" onClick={()=>showAreYouSureC(cart.id)} style={{'marginRight':'.5em'}}>Delete</button>
-                                </td>
-                            </tr>
-                            )
-                        }
+
+                        <Pagi user = {data.carts} itm="cart" />
                         </tbody>
                         </table> 
                         : <div className="emptyMsg" >Nothing Here</div>
